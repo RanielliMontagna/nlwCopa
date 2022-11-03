70e59c66558cfdbb65bf34e5bd484f44f8504b25
Ranielli Montagna <raniellimontagna@hotmail.com>
2022-11-03 17:42:58 -0300

    feat(mobile): faz implementação com o backend e sistema de roteamento

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { NativeBaseProvider, StatusBar } from 'native-base';
+import { Box, NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
-
-import { SignIn } from './src/screens/SignIn';
-import { New } from './src/screens/New';
-import { Find } from './src/screens/Find';
-import { Pools } from './src/screens/Pools';
 
 import { THEME } from './src/styles/theme';
 import { Loading } from './src/components/Loading';
 import { AuthContextProvider } from './src/context/AuthContext';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,11 +15,13 @@ export default function App() {
   });
 
   return (
-    <NativeBaseProvider theme={THEME}>
-      <AuthContextProvider>
-        <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-        {!fontsLoaded ? <Loading /> : <SignIn />}
-      </AuthContextProvider>
-    </NativeBaseProvider>
+    <Box flex={1} bgColor="gray.900">
+      <NativeBaseProvider theme={THEME}>
+        <AuthContextProvider>
+          <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+          {!fontsLoaded ? <Loading /> : <Routes />}
+        </AuthContextProvider>
+      </NativeBaseProvider>
+    </Box>
   );
 }
